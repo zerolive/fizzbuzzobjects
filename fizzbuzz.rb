2b53a8c731a721d8d2af3e4f5169c645c448107d
ZeroLive <zerolive@gmail.com>
2015-10-20 14:21:23 +0200

    muchas cosas hechas

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,47 +1,91 @@
 class FizzBuzz
 
-	class << self
+  class << self
 
-		def build fizzbuzznumber
-			raise "#{fizzbuzznumber} is not a FizzBuzz number" if is_fizzbuzz_number?(fizzbuzznumber).count != 2
-		end
-
-
+    def build fizzbuzznumber, cap=nil
+      raise "#{fizzbuzznumber} is not a FizzBuzz number" if is_fizzbuzz_number?(fizzbuzznumber).count != 2
+      if !cap.nil?
+        return create_return_string(fizzbuzznumber, cap)
+      end
+    end
 
     private
 
     def is_fizzbuzz_number? fizzbuzznumber
-    	@buzznumfizznum = []
-    	looking_quantity_divisors(fizzbuzznumber)
-    	return @buzznumfizznum
+      @buzznumfizznum = []
+      looking_quantity_divisors(fizzbuzznumber)
+      return @buzznumfizznum
     end 
 
     def looking_quantity_divisors fizzbuzznumber
-    	@divisors = fizzbuzznumber
-    	while @divisors > 2
-    		@divisors -= 1
-    		adding_divisors(fizzbuzznumber)
-    	end
+      @divisors = fizzbuzznumber
+      while @divisors > 2
+        @divisors -= 1
+        adding_divisors(fizzbuzznumber)
+      end
     end
 
     def adding_divisors fizzbuzznumber
-    		if fizzbuzznumber % @divisors == 0
-    			@buzznumfizznum << @divisors
-    		end
+        if (fizzbuzznumber % @divisors).zero?
+          @buzznumfizznum << @divisors
+        end
     end
 
-	end
+    def create_return_string fizzbuzznumber, cap 
+        textreturn = NumberFizzBuzz.build(fizzbuzznumber, cap) if textreturn.nil?
+        textreturn = NumberFizz.build(fizzbuzznumber, cap) if textreturn.nil?
+        textreturn = NumberBuzz.build(fizzbuzznumber, cap) if textreturn.nil?
+        textreturn = "#{cap} is just a number" if textreturn.nil?
+        return textreturn
+    end
+  end
 
 end
 
-class NumberFizz
+class NumberFizz < FizzBuzz
+
+  class << self
+
+    def build fizzbuzznumber, cap
+      numfizz = is_fizzbuzz_number?(fizzbuzznumber).last
+      "#{cap} is a Fizz number" if (cap % numfizz).zero?
+    end
+
+    private
+
+  end
 
 end
 
-class NumberBuzz
+class NumberBuzz < FizzBuzz
+
+  class << self
+
+    def build fizzbuzznumber, cap
+      numbuzz = is_fizzbuzz_number?(fizzbuzznumber).first
+      "#{cap} is a Buzz number" if (cap % numbuzz).zero?
+    end
+
+    private
+
+  end
 
 end
 
-class NumberFizzBuzz
+class NumberFizzBuzz < FizzBuzz
+
+  class << self
+
+    def build fizzbuzznumber, cap
+      numbuzz = is_fizzbuzz_number?(fizzbuzznumber).first
+      numfizz = is_fizzbuzz_number?(fizzbuzznumber).last
+      return "#{cap} is a FizzBuzz number" if (cap % numbuzz).zero? && (cap % numfizz).zero?
+    end
+
+    private
+
+  end
 
 end
+
+#FizzBuzz.build(15, 20)
--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -8,5 +8,32 @@ describe "The FizzBuzz" do
 		expect{(FizzBuzz.build(15))}.not_to raise_error
 		expect{(FizzBuzz.build(20))}.to raise_error "20 is not a FizzBuzz number"
 	end
+
+	it "returns a number" do
+		expect(FizzBuzz.build(15, 101)).to eq("101 is just a number")
+	end
+
+	it "does FizzBuzz" do
+		expect(FizzBuzz.build(15, 20)).to eq("20 is a Buzz number")
+		expect(FizzBuzz.build(15, 15)).to eq("15 is a FizzBuzz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("18 is a Fizz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("17 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("16 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("15 is a FizzBuzz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("14 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("13 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("12 is a Fizz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("11 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("10 is a Buzz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("9 is a Fizz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("8 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("7 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("6 is a Fizz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("5 is a Buzz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("4 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("3 is a Fizz number")
+		#expect(FizzBuzz.build(15, 20)).to eq("2 is just a number")
+		#expect(FizzBuzz.build(15, 20)).to eq("1 is just a number")
+	end
 	
 end
